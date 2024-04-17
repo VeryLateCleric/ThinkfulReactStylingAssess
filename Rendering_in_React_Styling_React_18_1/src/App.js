9df0c9c92add7e632650f774cb532d58bc439c49
Kenneth <kennethyoung.2960@gmail.com>
2024-04-16 21:37:45 -0500

    Initial commit 0 of 5 tests passing

---
+++ Rendering_in_React_Styling_React_18_1/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    // Do not delete: role="container"
+    <div role="container">
+      <h1>Hello!</h1>
+      <h4>How are you?</h4>
+      <h5>Today is a nice day!</h5>
+    </div>
+  );
+}
+
+export default App;
